9ac3c3b3175138acac536d31702ef338050eac18
yamaguchikatsuhiro <yamaguchi1497171@gmail.com>
2022-11-15 17:59:19 +0900

    Commit

---
+++ photo/forms.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from django.forms import ModelForm
+from .models import PhotoPost
+
+class PhotoPostForm(ModelForm):
+    
+    class Meta:
+        
+        model = PhotoPost
+        fields = ['category', 'title', 'comment', 'image1', 'image2']
